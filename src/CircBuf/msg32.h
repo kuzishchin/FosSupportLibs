cab063161536b492b231fa809043f44e91799dcb
kuzishchin <160040680+kuzishchin@users.noreply.github.com>
2026-02-06 16:51:40 +0300

    Add sign to msg32
    Add sign to msg32 to the file header.

--- src/CircBuf/msg32.h
+++ src/CircBuf/msg32.h
@@ -1,9 +1,24 @@
+/**************************************************************************//**
+ * @file      msg.h
+ * @brief     Message buffer for 32 bits data. Header file.
+ * @version   V1.0.00
+ * @date      06.02.2026
+ ******************************************************************************/
 /*
- * msg32.h
- *
- *  Created on: 19 янв. 2026 г.
- *      Author: yriyk
- */
+* Copyright 2024 Yury A. Kuzishchin and Vitaly A. Kostarev. All rights reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 
 #ifndef CIRCBUF_MSG32_H_
 #define CIRCBUF_MSG32_H_
